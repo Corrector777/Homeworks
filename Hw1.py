46a919389b1565e1394fa709d5eb55838cf5bca2
roman <rlysogor84@gmail.com>
2025-01-30 00:42:22 +0300

    try/exept

--- Hw1.py
+++ Hw1.py
@@ -4,13 +4,16 @@ import random
 count = 1
 random_number = random.randint(1, 100)
 while True:
-    my_number = int(input("Введите число: "))
-    if my_number > random_number:
-        count += 1
-        print('Ваше число больше')
-    elif my_number < random_number:
-        count += 1
-        print('Ваше число меньше сгенерированного')
-    else:
-        print(f'Верно! Ваше число = {my_number}. Вы угадали его с {count}й попытки')
-        break
+    try:
+        my_number = int(input("Введите число: "))       
+        if my_number > random_number:
+            count += 1
+            print('Ваше число больше')
+        elif my_number < random_number:
+            count += 1
+            print('Ваше число меньше сгенерированного')
+        else:
+            print(f'Верно! Ваше число = {my_number}. Вы угадали его с {count}й попытки')
+            break
+    except ValueError:
+        print('Ай-яй!! Принимаю только численные значения')
