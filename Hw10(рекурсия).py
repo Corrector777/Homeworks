0d8ff0797b728df992d69ab70605f9be9259760c
roman <rlysogor84@gmail.com>
2025-03-05 03:15:43 +0300

    10

--- Hw10(рекурсия).py
+++ Hw10(рекурсия).py
@@ -1,16 +1,13 @@
 def find_signal(room) -> bool:     # НЕ ЗНАЮ как написать анотацию на 2 возможных типа(строка/лист)
     if room == 'сигнал':
         return True
-    if room == 'тишина':
-        return False
   
-    if isinstance(room, list):
+    elif isinstance(room, list):
         for signal in room:          
             find_signal(signal)
         return find_signal(signal)
         
-    # else:
-    #     print('Error')
+    return False
     
 
 bunker = [
