f8b8dd3318c71e0419ad73feac9de67795946915
roman <rlysogor84@gmail.com>
2025-03-07 01:01:45 +0300

    Homework 10-2

---
+++ Homework10-2.py
@@ -0,0 +1,56 @@
+def echo(message: str, count: int) -> print:
+    '''Функция ЭХО. 
+    ARGS:сообщение(строка), кол-во повторений(int)
+    Вывод: выводит на печать сообщение заданное 
+    кол-во раз в пронумерованном порядке'''
+    if count <= 0:
+        pass
+    elif count > 0:        
+        echo(message, count - 1)
+        print(f'Echo {count}: {message}')
+
+
+# message = "Help me!"
+# count = 3
+# echo(message, count)
+
+# Задание 2
+
+
+def thread(length: int) -> print:
+    '''Функция выводит размер каждой петли нити
+        от заданной длины до 1
+        ARG: длина(int)
+        Вывод: размер каждой петли нити
+        от заданной длины до 1 '''
+    if isinstance(length, int):
+        if length <= 0:
+            pass
+        elif length > 0:
+            print(f'Thread loop of size: {length}')
+            thread(length - 1)
+    else:
+        print("Ошибка ввода")
+
+# length = 5
+# thread(length)
+
+
+# Задание 3: «Следы Минотавра»
+
+def minotaur_steps(tracks: int):
+    '''Функция считает и возвращает общее количество шагов
+      Минотавра по числу следов.
+      ARG: число следов (int)
+      Возвращает кол-во шагов''' # а именно самый первый вызов 
+      #рекурсивной функции minotaur_steps(tracks). Сначала функция рекурсивно вызывает
+      #  сама себя до тех пор, пока tracks не будет равен 0.Затем в обратном порядке возвращает из стека значения
+      #всех вызванных собой же функций и возвращает их значения по очереди.Последнее вернувшееся значение из стека вызовов(первая вызванная рекурсивная функция)
+      #  и есть наше искомое кол-во шагов
+    if tracks <= 0:
+        pass
+    elif tracks > 0:
+        minotaur_steps(tracks - 1)
+        return f'Total steps: {tracks}'
+    
+print(minotaur_steps(15))
