f5ca9bc8e715022a28ef76285d28cc3794ee4037
roman <rlysogor84@gmail.com>
2025-03-11 18:18:05 +0300

    Задача 2, 3 выполнена

--- Homework11(генераторы).py
+++ Homework11(генераторы).py
@@ -1,24 +1,68 @@
 import random
 
-items = ['меч', 'щит', 'зелье', 'ключ']
-riddles = ['Острое и блестящее', 'Защитит от удара', 'Восстанавливает силы',
-'Открывает любые двери']
+# # Задание 1: Загадочный сундук
+
+# items = ['меч', 'щит', 'зелье', 'ключ']
+# riddles = ['Острое и блестящее', 'Защитит от удара', 'Восстанавливает силы',
+# 'Открывает любые двери']
 
 
-# my_dict = {key: value for key in items for value in riddles}
+# pairs_list = []
+# for i in range(len(items)):
+#     pairs_list.append((items[i], riddles[i]))
+# random.shuffle(pairs_list)
+# iterator = iter(pairs_list)
+
+# try:
+#     print(next(iterator))
+#     print(next(iterator))
+#     print(next(iterator))
+#     print(next(iterator))
+#     print(next(iterator))
+# except StopIteration:
+#     print("Сундук пуст, поиск окончен!")
 
 
-def my_first_iterator():
-    for _ in range(len(items)):
-        index = random.choice(range(len(items)))
-        yield (items[index], riddles[index])
-    yield "Сундук пуст, поиск окончен!"
+# Задание 2: Генератор магических заклинаний
+magic_symbols = ['@', '#', '%', '&', '*']
+times = 5
+start_word = 'магия'
 
-iterator = iter(my_first_iterator())
 
-print(next(iterator))
-print(next(iterator))
-print(next(iterator))
-print(next(iterator))
-print(next(iterator))
+def magic_spell(start_word):
+    '''Генератор слов. Принимает стартовое слово в виде строки.
+       перемешивает буквы слова, добавляет спецсимвол и возвращает полученное значение 
+     в виде новой строки'''
+    current_word = start_word  # задаем значение текущего слова для дальнейшего изменения  
+    while True:    # Использую бесконечный цикл для генератора, так как кол-во вызовов генератора 
+                    #определяется циклом вызова генератора. При этом возможность случайной генерации при 
+                    # увеличении числа вызовов генератора не останавливалась
+        word_symb = [symb for symb in current_word]  # создаем список символов из стартовой строки
+        random.shuffle(word_symb)  # Перемешиваем символы
+        shuffled_word = ''.join(word_symb) # сохраняем новую строку из перемешанных символов
+        new_spell = shuffled_word + random.choice(magic_symbols)  # добавляем спецсимвол
+        current_word = new_spell # обновляем значение текущего слова для последующих итераций
+        yield new_spell
+        
 
+# my_gen = magic_spell(start_word)
+# for _ in range(times):    
+#     print(next(my_gen))
+
+# Задание 3: Гномы-шахтёры (соревнование)
+# dwarfs = ['Бофур', 'Кили', 'Ори']
+
+
+# def winner_dwarf(dwarf_list, current_round=1):
+#     '''Генератор определения победителя. Принимает список игороков.
+#        возвращает имя победителя и кол-во собранных алмазов'''
+#     while True:
+#         result_dict = {dwarf: random.choice(range(10)) for dwarf in dwarf_list}  # создадим словарь ключ-имя, значение- случ цифра от 0 до 9  
+#         winner = max(result_dict, key=result_dict.get)
+#         yield f'Раунд {current_round}: Лидер - {winner} ({result_dict[winner]} алмазов) '
+#         current_round += 1
+
+
+# winner = winner_dwarf(dwarfs)
+# for i in range(4):
+#   print(next(winner))
