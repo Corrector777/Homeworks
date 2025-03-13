50e7ecef86110b4214333017047a734d639e2f62
roman <rlysogor84@gmail.com>
2025-03-14 02:09:33 +0300

    Homework 12(исправлено после проверки)

--- Homework 13(файлы).py
+++ Homework 13(файлы).py
@@ -1,2 +1,15 @@
 # Задание 1. Расшифровка журнала подсказок
 
+try:
+    file = open('clues.log', 'r')
+    for line in file:
+        if 'подсказка' in line.lower():
+            print(line)
+except FileNotFoundError:
+    print('Ошибка! Файл не найден.')
+finally:
+    if file:
+        file.close()
+
+# Задание 2. Выделение критичных ошибок из журнала событий
+
