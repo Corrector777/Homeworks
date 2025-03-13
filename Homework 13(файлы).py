bf4cf6a0aa2aac66b63cca9d02a87d974703980f
roman <rlysogor84@gmail.com>
2025-03-14 02:39:42 +0300

    Homework 13 задача 1-2

--- Homework 13(файлы).py
+++ Homework 13(файлы).py
@@ -1,15 +1,32 @@
 # Задание 1. Расшифровка журнала подсказок
-
-try:
-    file = open('clues.log', 'r')
-    for line in file:
-        if 'подсказка' in line.lower():
-            print(line)
-except FileNotFoundError:
-    print('Ошибка! Файл не найден.')
-finally:
-    if file:
-        file.close()
+# file = None  # избегаем ошибки NameError: name 'file' is not defined в блоке finally(если файла нет) 
+# try:
+#     file = open('clues.log', 'r')
+#     for line in file:
+#         if 'подсказка' in line.lower():
+#             print(line.strip())
+# except FileNotFoundError:
+#     print('Ошибка! Файл не найден.')
+# finally:
+#     if file:
+#         file.close()
 
 # Задание 2. Выделение критичных ошибок из журнала событий
+# file = None
+# errors_list = []
 
+# try:
+#     file = open('log.txt', 'r')
+#     for line in file:
+#         if 'ERROR' in line:
+#             errors_list.append(line)
+#     if errors_list:
+#         error_file = open('errors.txt', 'w')
+#         error_file.writelines(errors_list)
+# except FileNotFoundError:
+#     print('Ошибка! Файл не найден.')
+# finally:
+#     if file:
+#         file.close()
+#     if error_file:
+#         error_file.close()
