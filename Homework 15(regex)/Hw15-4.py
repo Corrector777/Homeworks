11ecaaf14ab7eac936f23098d44e008fe0458928
roman <rlysogor84@gmail.com>
2025-03-31 15:33:30 +0300

    Задача 4 переработан

--- Homework 15(regex)/Hw15-4.py
+++ Homework 15(regex)/Hw15-4.py
@@ -3,6 +3,14 @@ import re
 
 def standardize_filenames(filenames):
     standardized_names = []
+    doc_type_map = {
+'отчет': 'ОТЧЕТ',
+'договор': 'ДОГОВОР',
+'счет': 'СЧЕТ',
+'презентация': 'ПРЕЗЕНТАЦИЯ',
+'документация': 'ДОКУМЕНТАЦИЯ',
+'анализ': 'АНАЛИЗ'
+}
 # Скомпилированные регулярные выражения для оптимизации
     # TODO: Создайте регулярные выражения для извлечения типа документа
     type_pattern = re.compile(r"документация|отчет|договор|счет|презентация|анализ", re.IGNORECASE)
@@ -12,20 +20,24 @@ def standardize_filenames(filenames):
     date_pattern = re.compile(r'(\d{2}).(\d{2}).(\d{4})')
     # print(date_pattern)
     # TODO: Создайте регулярное выражение для извлечения ID проекта
-    project_pattern = re.compile(r'[A-Z]{3}\d{3}')
+    project_pattern = re.compile(r'(?:проект[ау]?\s+|по_проекту_)?([A-Z]{3}\d{3})')
     # print(project_pattern)
     # TODO: Создайте регулярное выражение для извлечения автора/отдела    
     department_pattern = re.compile(r'отдел_аналитики|маркетинг|отдел_продаж|отдел_разработки', re.IGNORECASE)
-    author_pattern = re.compile(r'(?<=\()\w+(?=\)\.)|^[А-Я]\w+?(?=\_)|(?<=-для-)[A-Z]{1,}.[A-Z][a-z]+')
+    author_in_parentheses = re.compile(r'\(([а-яА-Я]+)\)')
+    author_in_begin = re.compile(r'^([а-яА-Я]+)_')
+    author_after_for = re.compile(r'для.([а-яА-Я]+)+')
+    athor_in_end = re.compile(r'\s+([а-яА-Я]+)\.')
     for filename in filenames:
         # print(filename)
         # Извлекаем расширение файла
         extension = filename.split('.')[-1]
         # print(extension)
         # TODO: Определите тип документа
-        doc_type = type_pattern.findall(filename)
-        if doc_type:
-            doc_type = doc_type[0].upper()  
+        doc_type_match = type_pattern.search(filename)
+        if doc_type_match:
+            found_type = doc_type_match.group(0).lower()
+            doc_type = doc_type_map.get(found_type, found_type.upper())   
         else:
             doc_type = "НЕИЗВЕСТЕН"
         # print(doc_type)
@@ -37,19 +49,31 @@ def standardize_filenames(filenames):
             date = "БЕЗ-ДАТЫ"
         # print(date)
         # TODO: Извлеките ID проекта
-        project_id = project_pattern.search(filename)
-        if project_id:
-            project_id = project_id.group(0)
-        else:
-            project_id = "НЕИЗВЕСТЕН"
+        project_match = project_pattern.search(filename)
+        # 7. Использование современных языковых возможностей
+        #     Конструкция next((g for g in project_match.groups() if g))
+        #     демонстрирует элегантное применение генераторов и функции next() для поиска
+        #     первого непустого значения.
+        # print('project_id', project_match)
+        # if project_match:
+        #     project_id = next(g for g in project_match.groups() if g)
+        # else:
+        #     project_id = "НЕИЗВЕСТЕН"
+
+        # при упрощенном регулярном выражении(как сейчас):
+        project_id = project_match.group(1) if project_match else "НЕИЗВЕСТЕН"    
         # print(project_id)
         # TODO: Извлеките автора/отдел
+        for pattern in [author_in_parentheses, author_in_begin, author_after_for, athor_in_end]:
+            match = pattern.search(filename)
+            if match:
+                author = match.group(1)
+                break
         department = department_pattern.search(filename)
-        author = author_pattern.search(filename)
         if department:
             department_author = department.group(0).upper()
         elif author:
-            department_author = author.group(0).upper()
+            department_author = author.upper()
         else:
             department_author = "НЕИЗВЕСТЕН"
         # print(department_author)
