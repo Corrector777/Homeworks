96b3deb301dbbed07bba925e45e484141f9e801c
roman <rlysogor84@gmail.com>
2025-04-23 15:57:36 +0300

    Задача 3

--- Homework 20 (loggin p.2)/Hw 20-3.py
+++ Homework 20 (loggin p.2)/Hw 20-3.py
@@ -1,9 +1,3 @@
-import gzip, time
-import os
-import logging
-from logging.handlers import RotatingFileHandler
-
-
 ## **Задание 3. “Архив времён”**
 
 # **ТЗ:**
@@ -17,28 +11,36 @@ from logging.handlers import RotatingFileHandler
 # 8. Симуляция: 30 ошибок с паузой.
     
 
-НЕ РАБОТАЕТ!
-Кол-во архивов явно больше 3х(выясненно ниже!)
-Файл battle.log не удаляется(по логике оно и не может удалиться ротатором.Так как в него идет основной лог.)
-NAMER принимает name = 'battle.log.1'(с цифрой на конце)! Специально запринтил
-source в ROTATOR передается как battle.log (БЕЗ цифры на конце). Специально запринтил!
-Размер MaxBytes = 1*102  - СПЕЦИАЛЬНО уменьшил для наглядности
-Количество симуляций СПЕЦИАЛЬНО увеличил
-____________________________________
-ИТАК ВЫЯСНЕНО!!! NAMER работает так: Если мы создаем с кастомным неймером УНИКАЛЬНЫЕ ИМЕНА файлов(как в случае со временем),
-то каждый раз при ротации будут создаваться НОВЫЕ файлы с кастомными именами(их кол-во ограничено только кол-вом общим кол-вом ротаций) и никак не привязано к backupCount  .
-А если эти имена будут шаблонными (как в случае с бэкапами log.1,log.2,log.3 и тд), то будет работать как надо(перезаписывая ровно нужное кол-во заданных бекапов- привязано к backupCount)
-____________________________________
+# НЕ РАБОТАЕТ!
+# Кол-во архивов явно больше 3х(выясненно ниже!)
+# Файл battle.log не удаляется(по логике оно и не может удалиться ротатором.Так как в него идет основной лог.)
+# NAMER принимает name = 'battle.log.1'(с цифрой на конце)! Специально запринтил
+# source в ROTATOR передается как battle.log (БЕЗ цифры на конце). Специально запринтил!
+# Размер MaxBytes = 1*102  - СПЕЦИАЛЬНО уменьшил для наглядности
+# Количество симуляций СПЕЦИАЛЬНО увеличил
+# ____________________________________
+# ИТАК ВЫЯСНЕНО!!! NAMER работает так: Если мы создаем с кастомным неймером УНИКАЛЬНЫЕ ИМЕНА файлов(как в случае со временем),
+# то каждый раз при ротации будут создаваться НОВЫЕ файлы с кастомными именами(их кол-во ограничено только кол-вом общим кол-вом ротаций) и никак не привязано к backupCount  .
+# А если эти имена будут шаблонными (как в случае с бэкапами log.1,log.2,log.3 и тд), то будет работать как надо(перезаписывая ровно нужное кол-во заданных бекапов- привязано к backupCount)
+# ____________________________________
+
+
+import gzip, time, glob
+import os
+import logging
+import pathlib
+from logging.handlers import RotatingFileHandler
+
+
+ARCHIEVE_LIMIT = 3
+
 
 def namer(name):
     print(f"NAMER: Received name = '{name}'")
     # TODO: разделить name на base и ext
-    # splitted_name = os.path.splitext(name)  НЕ РАБОТАЕТ! ДЕЛИТ НА battle.log и индекс 1-....
-    splitted_name = name.split('.')
-    base = splitted_name[0]
-    ext = splitted_name[1]
+    base, ext = os.path.splitext(name)  
     # TODO: собрать новое имя base + '.' + timestamp + ext
-    new_name = f'{base}.{time.strftime('%Y-%m-%d_%H-%M-%S')}.{ext}' 
+    new_name = f'{base}{time.strftime("%Y-%m-%d_%H-%M-%S")}'
     # TODO: вернуть новое имя
     print('NAMER: New name = ', new_name)
     return new_name
@@ -48,12 +50,16 @@ def rotator(source, dest):
     print(f"ROTATOR: Source file = '{source}', Dest file = '{dest}'")
     try:
         # TODO: открыть source для чтения в бинарном режиме
-        with open(source, 'rb') as f:
+        with open(source, 'rb') as f_in:
             # TODO: открыть gzip-архив dest+'.gz' для записи
-            with gzip.open(f"{dest}.gz", 'wb') as g:
+            with gzip.open(f"{dest}.gz", 'wb') as f_out:
                 # TODO: скопировать данные и удалить source
-                g.writelines(line for line in f)
-        os.remove(source)  # НЕ РАБОТАЕТ!!!
+                f_out.writelines(line for line in f_in)
+        os.remove(source)  # удаляет переполненный файл
+        # TODO: ограничить количество архивов
+        archives = sorted(glob.glob("battle.*.gz"), key=os.path.getmtime)
+        if len(archives) > ARCHIEVE_LIMIT:
+            os.remove(archives[0])
     except FileNotFoundError as e:
         print(e)
     except Exception as e:
@@ -66,7 +72,7 @@ def rotator(source, dest):
 #   - backupCount = 3
 handler = RotatingFileHandler(
     'battle.log',
-    maxBytes=1 * 102,
+    maxBytes=1 * 1024,
     backupCount=3
 )
 # TODO: присвоить handler.namer = namer и handler.rotator = rotator
@@ -82,14 +88,19 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 # --- Симуляция: 30 критических ошибок ---
-for i in range(50):
+for i in range(60):
     # TODO: вызвать logger.error с текстом "Critical battle error #{i}"
     logger.error(f"Critical battle error #{i}")
     print(f"Simulate critical battle error #{i}")
     time.sleep(0.1)
 
-
-# if os.path.exists('battle.log'):
-#     os.remove('battle.log')
+main_path = '/Users/roman/Git/Vs-Code/Ferapontov/'
 
 
+log_path = pathlib.Path(main_path).glob('*.log') 
+for path in log_path:
+    path.unlink()
+
+gz_path = pathlib.Path(main_path).glob('*.gz')
+for path in gz_path:
+    path.unlink()
