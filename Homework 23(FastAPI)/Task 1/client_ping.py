bfba7c8a99ee4d20c096447fa0fccdb091eaa579
roman <rlysogor84@gmail.com>
2025-05-13 01:11:56 +0300

    Задача 1

--- Homework 23(FastAPI)/Task 1/client_ping.py
+++ Homework 23(FastAPI)/Task 1/client_ping.py
@@ -1,10 +1,11 @@
 import requests
 
-exit = {"exit", "Exit", 'x', 'ч'}
+exit = {"нет", "no", 'н', 'n'}
 
 while True:
-    station_code = input("Input station code: ")
-    if station_code in exit:
-        break
+    station_code = input("Input station code: ").lower()
     response = requests.get(f"http://127.0.0.1:8000/ping/{station_code}")
     print(response.json())
+    break_point = input('\nПродолжаем(да = любой ввод/нет)?\n')
+    if break_point in exit:
+        break
