d93a0372861085be645b4a7f8a486e6752456a63
roman <rlysogor84@gmail.com>
2025-05-13 00:58:50 +0300

    Задача 2

--- Homework 23(FastAPI)/Task 2/client_channels.py
+++ Homework 23(FastAPI)/Task 2/client_channels.py
@@ -1,26 +1,30 @@
 import requests
 
-exit = {"exit", "Exit", 'x', 'ч'}
+exit = {"нет", "no", 'н', 'n'}
 
 while True:
 
     station_code = input("Введите имя канала: ").lower()
-    url = "http://127.0.0.1:8000/channels/{station_code}"
-    if station_code in exit:
-        print("До скорых встреч")
-        break
+    if len(station_code) < 1:
+        print("Ошибка! Ввод не может быть пустым")
+        continue
+    url = f"http://127.0.0.1:8000/channels/{station_code}"
 
-    channel_name = input("Вывести только активные каналы? (да/нет): ").lower()
-    match channel_name:
+    channels_status = input("Вывести только активные каналы? (да/нет(=пустой ввод)): ").lower()
+    
+    match channels_status:
         case 'да':
             print("\nАктивные каналы")
             response = requests.get(url, params={"active_only": True})
             print(response.json())
         case 'нет' | '':
-            print("\nВсе каналы")
+            print("\nВсе каналы")      
+
             response = requests.get(url, params={"active_only": False})
             print(response.json())
         case _:
             print("Некорректный ввод")
-    print('\nПродолжаем\n')
+    break_point = input('\nПродолжаем(нет/любой ввод = да)?\n')
+    if break_point in exit:
+        break
     
